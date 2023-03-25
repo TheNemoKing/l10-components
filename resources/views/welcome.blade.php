9ec635503a06ffe000a924d95a3f8df1302a8b7a
TheNemoKing <115928048+TheNemoKing@users.noreply.github.com>
2023-03-25 14:11:28 +0100

    valami

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,23 @@
+<h1>Anonymous components</h1>
 <x-layout>
-    <x-section>
-      Hello world 1x!
-    </x-section>
-    <x-section type="doubtful">
-      Hello world 2x!
-    </x-section>
-    <x-section type="unsuccessful" class="mt-10">
-      Hello world 3x!
-    </x-section>
-  </x-layout>
+  <x-section>
+    Hello world 1x!
+  </x-section>
+  <x-section type="doubtful">
+    Hello world 2x!
+  </x-section>
+  <x-section type="unsuccessful" class="mt-10">
+    Hello world 3x!
+  </x-section>
+</x-layout>
+<h1>Class based components</h1>
+<div class="m-8">
+  <x-alert title="Danger" message="Something not ideal might be happening." type="danger">
+    <a href="#" class="text-blue-700 underline">Danger weblink</a>
+  </x-alert>
+</div>
+<div class="m-8">
+  <x-alert title="Information" message="Everything is OK." type="success">
+    <a href="#" class="text-blue-700 underline">Success weblink</a>
+  </x-alert>
+</div>
